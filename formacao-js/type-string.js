ead752b8f67850d7939d1a2be4f5cc7df849fd20
Lucas Sales <marx.s.lukas@gmail.com>
2022-10-20 16:36:19 -0300

    formacao js

---
+++ formacao-js/type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo!';
+const senha = 'senhaSuperSegura456!';
+const stringDeNumeros = "34567";
+
+const citacao = "O leo disse 'oi!'";
+console.log(citacao);
+
+// concatenação (+)
+
+const fala = "Meu nome é "
+const meuNome = "Leonardo"
+
+console.log(fala + meuNome);
+
+//template string ou template literal
