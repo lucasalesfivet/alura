754362f8c0d19ef0ada787394cf03da1cebaabc6
Lucas Sales <marx.s.lukas@gmail.com>
2023-03-06 16:08:49 -0300

    ajuste do js barra lateral

--- html-css/opcentral-rh/candidatos.php
+++ html-css/opcentral-rh/candidatos.php
@@ -230,9 +230,7 @@ if (!isset($_SESSION['usu']))
 	</div>
 
 	<script src="js/app.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
-
-<script src="js/app.js"></script>
