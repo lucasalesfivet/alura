cd632eafb10ba2cfb493217d9a7583e6adceba1c
Lucas Sales <marx.s.lukas@gmail.com>
2023-03-15 09:54:22 -0300

    arquivo de conexao

--- html-css/opcentral-rh/connect/conexao.php
+++ html-css/opcentral-rh/connect/conexao.php
@@ -1,27 +1,3 @@
-<?php 
-$ora_user = "CONSULTA";
-$ora_senha = "CONSULTA";
-
-$ora_bd = 
-
-    "(DESCRIPTION=
-          (ADDRESS_LIST=
-            (ADDRESS=(PROTOCOL=TCP) 
-              (HOST=192.168.2.197)(PORT=1521)
-            )
-          )
-          (CONNECT_DATA=(SERVICE_NAME=wint))
-     )"; 
-
-$conexao = oci_connect($ora_user, $ora_senha, $ora_bd);
-/*
-if ($ora_conexao = oci_connect($ora_user, $ora_senha, $ora_bd)){
-    echo "Conectado!";
-} else {
-    echo "Não conectado!";
-}*/
-?>
-
 <?php
 
 
