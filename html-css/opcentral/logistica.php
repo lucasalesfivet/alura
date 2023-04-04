bd9c3eed21371b2c5df3ab9a6f61f99da8933d00
Lucas Sales <marx.s.lukas@gmail.com>
2023-04-04 11:14:31 -0300

    painel logistica

--- html-css/opcentral/logistica.php
+++ html-css/opcentral/logistica.php
@@ -7,6 +7,9 @@ session_start();
 if (!isset($_SESSION['usu']))
 
 	header("Location: index.php?op=err");
+
+include 'connect/conexao.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +17,7 @@ if (!isset($_SESSION['usu']))
 
 <head>
 
-<?php include 'header.php'; ?>
+	<?php include 'header.php'; ?>
 
 </head>
 
@@ -84,19 +87,261 @@ if (!isset($_SESSION['usu']))
 
 			<main class="content">
 				<div class="container-fluid p-0">
+
+					<h1 class="h3 mb-3"><strong>Logística</strong> Analytics</h1>
+
+
 					<div class="row">
-						<div class="col-sm-12">
+						<div class="col-sm-6">
 							<div class="card">
 								<div class="card-body">
 									<div class="row">
-										<div class="col mt-0" style="text-align: center; justify-content: center;">
-											<h1>Logística</h1>
+										<div class="col mt-0">
+											<h5 class="card-title">Caminhões em Rota</h5>
 										</div>
+										<div class="col-auto">
+											<div class="stat text-primary">
+												<i class="align-middle" data-feather="truck"></i>
+											</div>
+										</div>
+									</div>
+									<h1 class="mt-1 mb-3">
+										<?php
+										$sql = ("SELECT * FROM
+										(SELECT COUNT (codveiculo)QT_VEICULO_ATIVO
+										FROM pcveicul
+										WHERE situacao     <> 'I'
+										AND codveiculo NOT IN (0,1, 267)
+										) veiculo_ativo,
+										(SELECT COUNT (DISTINCT (codveiculo)) EMROTA
+										FROM pccarreg
+										WHERE dtfecha      IS NULL
+										AND destino NOT    IN ( 'VENDA BALCAO', 'CANCELADO')
+										AND codveiculo NOT IN (0,1,267)
+										AND dtsaida  >= '01-out-2022'
+										)veiculo_viagem");
+
+										$stid = oci_parse($conexao, $sql);
+										$execute = oci_execute($stid);
+
+										while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+											// Use the uppercase column names for the associative array indices
+											echo $row['EMROTA'];
+										}
+										?>
+									</h1>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="card">
+								<div class="card-body">
+									<div class="row">
+										<div class="col mt-0">
+											<h5 class="card-title">Caminhões Livres</h5>
+										</div>
+
+										<div class="col-auto">
+											<div class="stat text-primary">
+												<i class="align-middle" data-feather="truck"></i>
+											</div>
+										</div>
+									</div>
+									<h1 class="mt-1 mb-3">
+										<?php
+										$sql = ("SELECT * FROM
+											(SELECT COUNT (codveiculo) ATIVOS
+											FROM pcveicul
+											WHERE situacao     <> 'I'
+											AND codveiculo NOT IN (0,1, 267)
+											) veiculo_ativo,
+											(SELECT COUNT (DISTINCT (codveiculo)) EMROTA
+											FROM pccarreg
+											WHERE dtfecha      IS NULL
+											AND destino NOT    IN ( 'VENDA BALCAO', 'CANCELADO')
+											AND codveiculo NOT IN (0,1,267)
+											AND dtsaida  >= '01-out-2022'
+											)veiculo_viagem");
+
+										$stid = oci_parse($conexao, $sql);
+										$execute = oci_execute($stid);
+
+										while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+											// Use the uppercase column names for the associative array indices
+											echo $row['ATIVOS'] - $row['EMROTA'];
+										}
+										?>
+									</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="card">
+								<div class="card-body">
+									<div class="row">
+										<div class="col mt-0">
+											<h5 class="card-title">Palmas/Marabás</h5>
+										</div>
+										<div class="col-auto">
+											<div class="stat text-primary">
+												<i class="align-middle" data-feather="truck"></i>
+											</div>
+										</div>
+									</div>
+									<h1 class="mt-1 mb-3">
+										<?php
+										$sql = ("SELECT 'PALMAS_MARABA', 
+												count (ped.numped)qtd_ped,
+												SUM (ped.vlatend)VLTOTROTA,
+												SUM(distinct (r.vlmincarreg))VLMINROTAS  ,
+												ROUND (((SUM (ped.vlatend)/SUM(distinct (r.vlmincarreg)))*100),2) PERCMIN,
+												COUNT(ped.posicao) TOTPEDIDOS
+											FROM pcclient c
+											JOIN pcpraca p
+											ON p.codpraca = c.codpraca
+											JOIN pcrotaexp r
+											ON r.codrota = p.rota
+											JOIN pcpedc ped
+											ON ped.codcli     = c.codcli
+											WHERE ped.posicao = 'L' and r.codrota in (46,36) GROUP BY 'PALMAS_MARABA'");
+
+										$stid = oci_parse($conexao, $sql);
+										$execute = oci_execute($stid);
+
+										while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+											// Use the uppercase column names for the associative array indices
+											echo $row['PERCMIN'] . '%';
+										}
+										?>
+									</h1>
+									<div class="mb-0">
+										<span class="text-muted">
+											<?php
+											$stid = oci_parse($conexao, $sql);
+											$execute = oci_execute($stid);
+											while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+												// Use the uppercase column names for the associative array indices
+												echo $row['TOTPEDIDOS'] . ' pedidos';
+											}
+											?>
+										</span>
 									</div>
 								</div>
 							</div>
-					</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="card">
+								<div class="card-body">
+									<div class="row">
+										<div class="col mt-0">
+											<h5 class="card-title">Posse/Goiás Velho</h5>
+										</div>
 
+										<div class="col-auto">
+											<div class="stat text-primary">
+												<i class="align-middle" data-feather="truck"></i>
+											</div>
+										</div>
+									</div>
+									<h1 class="mt-1 mb-3">
+										<?php
+										$sql = ("SELECT 'POSSE_GOIASVELHO', 
+												count (ped.numped)qtd_ped,
+												SUM (ped.vlatend)VLTOTROTA,
+												SUM(distinct (r.vlmincarreg))VLMINROTAS  ,
+												ROUND (((SUM (ped.vlatend)/SUM(distinct (r.vlmincarreg)))*100),2) PercMin,
+												COUNT(ped.posicao) TOTPEDIDOS
+											FROM pcclient c
+											JOIN pcpraca p
+											ON p.codpraca = c.codpraca
+											JOIN pcrotaexp r
+											ON r.codrota = p.rota
+											JOIN pcpedc ped
+											ON ped.codcli     = c.codcli
+											WHERE ped.posicao = 'L' and r.codrota in (31,32) GROUP BY '1', 'POSSE_GOIASVELHO'");
+
+										$stid = oci_parse($conexao, $sql);
+										$execute = oci_execute($stid);
+
+										while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+											// Use the uppercase column names for the associative array indices
+											echo $row['PERCMIN'] . '%';
+										}
+										?>
+									</h1>
+									<div class="mb-0">
+										<span class="text-muted">
+											<?php
+											$stid = oci_parse($conexao, $sql);
+											$execute = oci_execute($stid);
+											while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+												// Use the uppercase column names for the associative array indices
+												echo $row['TOTPEDIDOS'] . ' pedidos';
+											}
+											?>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="card">
+								<div class="card-body">
+									<div class="row">
+										<div class="col mt-0">
+											<h5 class="card-title">Posse/Goiás Velho</h5>
+										</div>
+
+										<div class="col-auto">
+											<div class="stat text-primary">
+												<i class="align-middle" data-feather="truck"></i>
+											</div>
+										</div>
+									</div>
+									<h1 class="mt-1 mb-3">
+										<?php
+										$sql = ("SELECT 'POSSE_GOIASVELHO', 
+												count (ped.numped)qtd_ped,
+												SUM (ped.vlatend)VLTOTROTA,
+												SUM(distinct (r.vlmincarreg))VLMINROTAS  ,
+												ROUND (((SUM (ped.vlatend)/SUM(distinct (r.vlmincarreg)))*100),2) PercMin,
+												COUNT(ped.posicao) TOTPEDIDOS
+											FROM pcclient c
+											JOIN pcpraca p
+											ON p.codpraca = c.codpraca
+											JOIN pcrotaexp r
+											ON r.codrota = p.rota
+											JOIN pcpedc ped
+											ON ped.codcli     = c.codcli
+											WHERE ped.posicao = 'L' and r.codrota in (31,32) GROUP BY '1', 'POSSE_GOIASVELHO'");
+
+										$stid = oci_parse($conexao, $sql);
+										$execute = oci_execute($stid);
+
+										while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+											// Use the uppercase column names for the associative array indices
+											echo $row['PERCMIN'] . '%';
+										}
+										?>
+									</h1>
+									<div class="mb-0">
+										<span class="text-muted">
+											<?php
+											$stid = oci_parse($conexao, $sql);
+											$execute = oci_execute($stid);
+											while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+												// Use the uppercase column names for the associative array indices
+												echo $row['TOTPEDIDOS'] . ' pedidos';
+											}
+											?>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</main>
 
@@ -108,8 +353,8 @@ if (!isset($_SESSION['usu']))
 
 	<script src="js/app.js"></script>
 	<script>
-      feather.replace()
-    </script>
+		feather.replace()
+	</script>
 
 </body>
 
